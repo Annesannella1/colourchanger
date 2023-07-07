503cd4c214024efa0f250eee370d17ee0c9a87da
Anne Sannella <27774757+Annesannella1@users.noreply.github.com>
2023-07-07 14:00:52 -0500

    Update script.js

--- script.js
+++ script.js
@@ -147,7 +147,7 @@ var animate = anime({
   update: function() {
     ctx.fillStyle = bgColor;
     ctx.fillRect(0, 0, cW, cH);
-    ctx.font = '36px Brush Script';
+    ctx.font = '36px Comic Sans';
     ctx.fillStyle = 'black';
     ctx.textAlign = "start";   
       ctx.fillText( "Hurt", cW/2, cH/3 );    
@@ -162,11 +162,11 @@ var animate = anime({
    
     ctx.textAlign = "right";   
   
-    ctx.fillText( "and...", cW/2, cH/1 );    
+    ctx.fillText( "and...", cW/2, cH/2 );    
    
     ctx.textAlign = "center";   
   
-    ctx.fillText( "Let it go", cW/2, cH/.75 );
+    ctx.fillText( "Let it go", cW/2, cH/2 );
     
 
     
